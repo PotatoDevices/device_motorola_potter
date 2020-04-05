f58ef3460d2105a297d3bc21339800bcca05e18e
Ashwin R C <ashwin2001achu@gmail.com>
2020-04-05 08:54:29 +0000

    potter: posp bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_potter.mk
+    $(LOCAL_DIR)/potato_potter.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_potter-user \
-    arrow_potter-userdebug \
-    arrow_potter-eng
+    potato_potter-user \
+    potato_potter-userdebug \
+    potato_potter-eng
