4e247a05153902fd0da8b07350413f69c367846d
Zilberto Junior <priv.zjr@gmail.com>
2020-07-01 09:51:04 -0300

    Use the right Makefile
    Fix APN issue
    
    Signed-off by: ZJRDroid <zjrdroid@pixelexperience.org>

--- potato_potter.mk
+++ potato_potter.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common POSP stuff.
-$(call inherit-product, vendor/potato/config/common.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
